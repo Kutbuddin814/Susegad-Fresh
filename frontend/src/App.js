ff47923de4abea7ac4005d7f6f0a58e9f341dcf1
Kutbuddin814 <kutbuddinshaikh814@gmail.com>
2026-01-09 23:04:29 +0530

    Fix: converted submodules to regular folders

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
+import Login from "./pages/Login"; // 1. Import the Login component
 import Cart from "./pages/Cart";
 import Checkout from "./pages/Checkout";
 import Orders from "./pages/Orders";
@@ -12,17 +13,19 @@ function App() {
   return (
     <Router>
       <Navbar />
-
       <Routes>
         <Route path="/" element={<Home />} />
+        
+        {/* 2. Add the Login route here */}
+        <Route path="/login" element={<Login />} /> 
+        
         <Route path="/cart" element={<Cart />} />
         <Route path="/checkout" element={<Checkout />} />
         <Route path="/orders" element={<Orders />} />
         <Route path="/profile" element={<Profile />} />
-
-        ✅ {/* THIS WAS MISSING */}
         <Route path="/order-success" element={<OrderSuccess />} />
 
+        {/* This is what was showing because /login didn't exist above */}
         <Route path="*" element={<div className="p-10">Page not found</div>} />
       </Routes>
     </Router>
